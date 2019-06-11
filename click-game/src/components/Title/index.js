b4f44a78975d380fcd1fd3bb45db533c11f290ac
David Lawrence <dalaw85@gmail.com>
2019-06-11 16:03:58 -0400

    change styles

--- click-game/src/components/Title/index.js
+++ click-game/src/components/Title/index.js
@@ -4,9 +4,9 @@ import "./style.css";
 function Title(props) {
   return (
   <div className="title">
-    <h1 className="title">Clicky Game{props.children}</h1>
+    <h1 id="main-title">CLicky Game{props.children}</h1>
     <p id="rules">Click an image you have not already clicked to score a point. If you have already clicked the image you lose!</p>
-    <h3>Score: {props.count}</h3>
+    <h3 id="score">Score: {props.count}</h3>
   </div>
 
   )
